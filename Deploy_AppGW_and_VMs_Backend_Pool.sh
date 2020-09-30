edd6d69bcd54ec66e886d0a6688f57ade7fb5ba0
XaaSTechnologies <69765118+XaaSTechnologies@users.noreply.github.com>
2020-10-01 01:42:28 +0300

    Update Deploy_AppGW_and_VMs_Backend_Pool.sh

--- Deploy_AppGW_and_VMs_Backend_Pool.sh
+++ Deploy_AppGW_and_VMs_Backend_Pool.sh
@@ -1,21 +1,91 @@
-# Script to create Application Gateway and Backend Pool Virtual Machines (VMs) - VM-01, VM-02, VM-03
-
-az group create \
---name RG-13-DNS \
---location westeurope
+az group create --name RG-13-DNS --location westeurope
 
 az network vnet create \
---resource-group RG-13-DNS \
---name AZ104-vNET \
---address-prefix 10.0.0.0/16 \
---subnet-name APPGW-SUBNET \
---subnet-prefix 10.0.0.0/24
+  --name myVNet \
+  --resource-group RG-13-DNS \
+  --location westeurope \
+  --address-prefix 10.0.0.0/16 \
+  --subnet-name myAGSubnet \
+  --subnet-prefix 10.0.1.0/24
 
 az network vnet subnet create \
---address-prefixes 10.0.1.0/24 \
---name BACKEND-SUBNET \
---vnet-name AZ104-vNET \
---resource-group RG-13-DNS
+  --name myBackendSubnet \
+  --resource-group RG-13-DNS \
+  --vnet-name myVNet \
+  --address-prefix 10.0.2.0/24
+
+az network public-ip create \
+  --resource-group RG-13-DNS \
+  --name myAGPublicIPAddress \
+  --allocation-method Static \
+  --sku Standard
+
+az network application-gateway create \
+  --name myAppGateway \
+  --location westeurope \
+  --resource-group RG-13-DNS \
+  --vnet-name myVNet \
+  --subnet myAGsubnet \
+  --capacity 2 \
+  --sku Standard_v2 \
+  --http-settings-cookie-based-affinity Disabled \
+  --frontend-port 80 \
+  --http-settings-port 80 \
+  --http-settings-protocol Http \
+  --public-ip-address myAGPublicIPAddress
+
+az network application-gateway address-pool create \
+  --gateway-name myAppGateway \
+  --resource-group RG-13-DNS \
+  --name imagesBackendPool
+
+az network application-gateway address-pool create \
+  --gateway-name myAppGateway \
+  --resource-group RG-13-DNS \
+  --name videoBackendPool
+
+az network application-gateway frontend-port create \
+  --port 8080 \
+  --gateway-name myAppGateway \
+  --resource-group RG-13-DNS \
+  --name port8080
+
+az network application-gateway http-listener create \
+  --name backendListener \
+  --frontend-ip appGatewayFrontendIP \
+  --frontend-port port8080 \
+  --resource-group RG-13-DNS \
+  --gateway-name myAppGateway
+
+az network application-gateway url-path-map create \
+  --gateway-name myAppGateway \
+  --name myPathMap \
+  --paths /images/* \
+  --resource-group RG-13-DNS \
+  --address-pool imagesBackendPool \
+  --default-address-pool appGatewayBackendPool \
+  --default-http-settings appGatewayBackendHttpSettings \
+  --http-settings appGatewayBackendHttpSettings \
+  --rule-name imagePathRule
+
+az network application-gateway url-path-map rule create \
+  --gateway-name myAppGateway \
+  --name videoPathRule \
+  --resource-group RG-13-DNS \
+  --path-map-name myPathMap \
+  --paths /video/* \
+  --address-pool videoBackendPool
+
+az network application-gateway rule create \
+  --gateway-name myAppGateway \
+  --name rule2 \
+  --resource-group RG-13-DNS \
+  --http-listener backendListener \
+  --rule-type PathBasedRouting \
+  --url-path-map myPathMap \
+  --address-pool appGatewayBackendPool
+
+
 
 # NSG for VM-01
 az network nsg create \
@@ -56,9 +126,9 @@ az vm create \
   --admin-username adminuser \
   --admin-password adminadmin123! \
   --image UbuntuLTS \
-  --vnet-name AZ104-vNET \
+  --vnet-name myVNet \
   --nsg NSG-VM-01 \
-  --subnet BACKEND-SUBNET
+  --subnet myBackendSubnet
 
 az vm extension set \
   --publisher Microsoft.Azure.Extensions \
@@ -107,9 +177,9 @@ az vm create \
   --admin-username adminuser \
   --admin-password adminadmin123! \
   --image UbuntuLTS \
-  --vnet-name AZ104-vNET \
+  --vnet-name myVNet \
   --nsg NSG-VM-02 \
-  --subnet BACKEND-SUBNET
+  --subnet myBackendSubnet
 
 az vm extension set \
   --publisher Microsoft.Azure.Extensions \
@@ -117,7 +187,7 @@ az vm extension set \
   --name CustomScript \
   --vm-name VM-02 \
   --resource-group RG-13-DNS \
-  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && rm -rf /var/www/html && git clone https://github.com/XaaSTechnologies/AppGw-Images-Web-Server.github.io.git /var/www/html/images/”}'
+  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && rm -rf /var/www/html && git clone https://github.com/XaaSTechnologies/AppGw-Images-Web-Server.github.io.git /var/www/html/images"}'
 
 
 # NSG for VM-03
@@ -159,9 +229,9 @@ az vm create \
   --admin-username adminuser \
   --admin-password adminadmin123! \
   --image UbuntuLTS \
-  --vnet-name AZ104-vNET \
+  --vnet-name myVNet \
   --nsg NSG-VM-03 \
-  --subnet BACKEND-SUBNET
+  --subnet myBackendSubnet
 
 az vm extension set \
   --publisher Microsoft.Azure.Extensions \
@@ -169,90 +239,14 @@ az vm extension set \
   --name CustomScript \
   --vm-name VM-03 \
   --resource-group RG-13-DNS \
-  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && rm -rf /var/www/html && git clone https://github.com/XaaSTechnologies/AppGw-Video-Web-Server.github.io.git /var/www/html/video/”}'
-
-# Deploy Application Gateway
-
-az network public-ip create \
-  --resource-group RG-13-DNS \
-  --name AppGW-PIP \
-  --allocation-method Static \
-  --sku Standard
-
-az network application-gateway create \
-  --name APPGW \
-  --location westeurope \
-  --resource-group RG-13-DNS \
-  --vnet-name AZ104-vNET \
-  --subnet APPGW-SUBNET \
-  --capacity 2 \
-  --sku Standard_v2 \
-  --http-settings-cookie-based-affinity Disabled \
-  --frontend-port 80 \
-  --http-settings-port 80 \
-  --http-settings-protocol Http \
-  --public-ip-address AppGW-PIP
-
-az network application-gateway address-pool create \
-  --gateway-name APPGW \
-  --resource-group RG-13-DNS \
-  --name IMAGES-BACKEND-POOL
-
-az network application-gateway address-pool create \
-  --gateway-name APPGW \
-  --resource-group RG-13-DNS \
-  --name VIDEO-BACKEND-POOL
-
-az network application-gateway frontend-port create \
-  --port 8080 \
-  --gateway-name APPGW \
-  --resource-group RG-13-DNS \
-  --name FRONTEND-PORT-8080
-
-az network application-gateway http-listener create \
-  --name BACKEND-LISTENER \
-  --frontend-ip appGatewayFrontendIP \
-  --frontend-port FRONTEND-PORT-8080 \
-  --resource-group RG-13-DNS \
-  --gateway-name APPGW
+  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install apache2 && rm -rf /var/www/html && git clone https://github.com/XaaSTechnologies/AppGw-Video-Web-Server.github.io.git /var/www/html/video"}'
 
 
-az network application-gateway url-path-map create \
-  --gateway-name APPGW \
-  --name myPathMap \
-  --paths /images/* \
-  --resource-group RG-13-DNS \
-  --address-pool IMAGES-BACKEND-POOL \
-  --default-address-pool appGatewayBackendPool \
-  --default-http-settings appGatewayBackendHttpSettings \
-  --http-settings appGatewayBackendHttpSettings \
-  --rule-name IMAGES-PATH-RULE
+az network application-gateway address-pool update -g RG-13-DNS --gateway-name myAppGateway \
+-n appGatewayBackendPool --servers 10.0.2.4
 
-az network application-gateway url-path-map rule create \
-  --gateway-name APPGW \
-  --name IMAGES-PATH-RULE \
-  --resource-group RG-13-DNS \
-  --path-map-name myPathMap \
-  --paths /video/* \
-  --address-pool VIDEO-BACKEND-POOL
+az network application-gateway address-pool update -g RG-13-DNS --gateway-name myAppGateway \
+-n imagesBackendPool --servers 10.0.2.5
 
-az network application-gateway rule create \
-  --gateway-name APPGW \
-  --name rule2 \
-  --resource-group RG-13-DNS \
-  --http-listener BACKEND-LISTENER \
-  --rule-type PathBasedRouting \
-  --url-path-map myPathMap \
-  --address-pool appGatewayBackendPool
-
-
-#Add VMs to backend pools
-
-az network application-gateway address-pool update -g RG-13-DNS --gateway-name APPGW \ 
--n appGatewayBackendPool --servers 10.0.1.4
-
-az network application-gateway address-pool update -g RG-13-DNS --gateway-name APPGW \
--n IMAGES-BACKEND-POOL --servers 10.0.1.5
-
-az network application-gateway address-pool update -g RG-13-DNS --gateway-name APPGW \
--n VIDEO-BACKEND-POOL --servers 10.0.1.6
+az network application-gateway address-pool update -g RG-13-DNS --gateway-name myAppGateway \
+-n videoBackendPool --servers 10.0.2.6
